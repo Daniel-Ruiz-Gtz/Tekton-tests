c6dc166a080dc3d35df56aa529bec1f238892f8b
Daniel de la Cruz Ruiz Gutierrez <daniel.ruiz.gtz@ibm.com>
2025-08-14 16:14:32 -0600

    Setup test repo

---
+++ Main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello, welcome to this example program.")
+    name = input("What is your name? ")
+    print(f"Nice to meet you, {name}!")
+
+    number = int(input("Give me a number and I will double it: "))
+    print(f"Double of {number} is {number * 2}")
+
+if __name__ == "__main__":
+    main()
