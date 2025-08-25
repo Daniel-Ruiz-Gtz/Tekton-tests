ad90a7a53cbd31458da324f52e96cc1a65e8d86d
Daniel de la Cruz Ruiz Gutierrez <daniel.ruiz.gtz@ibm.com>
2025-08-25 00:08:50 -0600

    Update Main.py

--- testcases/Main.py
+++ testcases/Main.py
@@ -7,7 +7,7 @@ def main():
     print(f"Double of {number} is {number * 2}")
     print(f"NEW CHANGE 2")
     print(f"PR AND PUSH TO MAIN")
-    print(f"Testing Push main")
+    print(f"Testing Push main V2")
 
 if __name__ == "__main__":
     main()
